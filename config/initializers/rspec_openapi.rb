42fc3a6a902c23cd784e046c1743fb18c47a89a9
Michal Z <michalzynek@gmail.com>
2024-08-25 22:15:27 +0200

    [BUGFIX] Fix auth issues on api docs

--- config/initializers/rspec_openapi.rb
+++ config/initializers/rspec_openapi.rb
@@ -4,11 +4,18 @@ return unless Rails.env.test?
 
 require 'rspec/openapi'
 
-# Set request `headers` - generate parameters with headers for a request
-RSpec::OpenAPI.request_headers = %w[access-token uid client]
-
-# Set response `headers` - generate parameters with headers for a response
-RSpec::OpenAPI.response_headers = %w[access-token expiry token-type uid client]
-
 # Support generating the docs when running specs with `parallel_tests`
 RSpec::OpenAPI.path = ->(_) { "doc/openapi#{ENV.fetch('TEST_ENV_NUMBER', '')}.yaml" }
+
+RSpec::OpenAPI.title = 'Geolocation API'
+
+RSpec::OpenAPI.application_version = '1.2.1'
+
+RSpec::OpenAPI.security_schemes = {
+  'Authorization' => {
+    description: 'Authenticate API requests via API Token',
+    in: 'header',
+    name: 'Authorization',
+    type: 'apiKey'
+  }
+}
